818a2fc031927b01f45d4e4a4d38be4a5201abac
dlwjdcks11 <dlwjdcks7356@gmail.com>
2022-02-14 18:08:12 +0900

    Add gitignore file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "670036f0cfbd407e"; }
+/******/ 		__webpack_require__.h = function() { return "11ce1a9307d5799b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- a.py
+++
@@ -1,8 +0,0 @@
-def a(a, b):
-    """더하는 함수
-
-    Args:
-        a (int): a
-        b (int): b
-    """
-return a + b;
